e085e1673b469e0536acc0f8778ed06390266dbe
Soz <soz.raheem@ru.nl>
2025-05-09 22:06:14 +0200

    Move offline / calibration evaluation functions into utils/offline_evaluation.py; Add documentation

---
+++ ERP_analysis_code/patient_pipeline/utils/offline_evaluation.py
@@ -0,0 +1,275 @@
+# comparing LDA - BT-LDA - sLDA
+# data is in channel prime order
+# evaulation method added from Jan's example_toeplitz_lda_simple.py
+
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score
+from toeplitzlda.classification import ToeplitzLDA
+import numpy as np
+import warnings
+import mne
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, cross_val_score 
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+import sklearn.metrics as metrics
+
+
+# Turn off warnings (that most likely occur from ToeplitzLDA)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=RuntimeWarning)
+mne.set_log_level('WARNING')
+
+# See cell block in dump file for documentation on how this function works
+def get_jumping_means(epo, boundaries):
+    """Feature extraction by averaging over time intervals between the given 'boundaries' """
+    shape_orig = epo.get_data().shape
+    X = np.zeros((shape_orig[0], shape_orig[1], len(boundaries)-1))
+    for i in range(len(boundaries)-1):
+        idx = epo.time_as_index((boundaries[i], boundaries[i+1]))
+        idx_range = list(range(idx[0], idx[1]))
+        X[:,:,i] = epo.get_data()[:,:,idx_range].mean(axis=2)
+    return X
+
+def compare_auc_single_trial_interval(trials, start=0, stop=12, test_size=0.2, only_auc = True, ival_bounds = np.array([0.1, 0.2, 0.3, 0.4, 0.5]), plot_roc_curves = True):
+    """
+    compares LDA vs sLDA vs BT-LDA on calibration data using a single train-test-split
+    
+    calibration data is determined by a selection of trials [start - stop], e.g. [0-12]
+    note that this is not robust! the interval matters. You can check this by passing different values for [start - stop]
+    The full dataset here is the selected interval of calibration data. This interval will be split with sklearn's train_test_split method.
+
+    Parameters:
+    - trials (list): list of all trials
+    - start (int): first trial to take as calibration data
+    - stop (int): last trial to take as calibration data. if None, then stop is the final trial of trials 
+    - test_size (double): the test part in the ratio train:test when splitting the calibration data. This is a value between 0 and 1.
+    - only_auc (boolean): if True, only the auc scores are printed for all three classifiers. If False, then the balanced auc scores are printed too.
+    - ival_bounds (numpy array): time interval boundaries to average the raw calibration trials over
+    - plot_roc_curves (boolean): if True, also plot the ROC curves. if False, then only the AUC scores are printed. 
+    """
+
+    ### Feature extraction ------------------------------------------------------------
+
+    # Splitting the dataset
+    if stop is None:
+        stop = len(trials)
+    raw_calibration_trials = trials[start:stop] 
+
+    clf_ival_boundaries = ival_bounds
+
+    # Using lists instead of np arrays because of inconsistent amounts of iterations per trial 
+    calibration_trials = [[get_jumping_means(iteration, clf_ival_boundaries) for iteration in trial] for trial in raw_calibration_trials]
+    calibration_trials_reshaped = [
+        [epochs.transpose(0, 2, 1) for epochs in trial] # make channel prime
+        for trial in calibration_trials
+    ]
+
+    # Reshaping to feed into classifier
+    calibration_stimuli = list() # X 
+
+    # flatten into input for classifier
+    for trial in calibration_trials_reshaped:
+        for iteration in trial:
+            for epoch in iteration:
+                # flatten each epoch from 2D (n_time_ivals, n_channels) to 1D (n_time_ivals * n_channels) 
+                calibration_stimuli.append(epoch.reshape(-1)) 
+
+    calibration_stimuli = np.array(calibration_stimuli) # shape (samples,features),  e.g. (1038, 124)
+
+    # obtain flattened labels 
+    calibration_labels = list() # y
+    for trial in raw_calibration_trials:
+        for iteration in trial:
+            for event in iteration.events[:,2]:
+                calibration_labels.append(1 if event > 107 else 0)
+    
+    calibration_labels = np.array(calibration_labels) # contains only 0's and 1's. # shape (samples,) e.g. (1038,)
+
+    ### Evaluation --------------------------------------------------------------------
+
+    X_train, X_test, y_train, y_test = train_test_split(calibration_stimuli, calibration_labels, test_size=test_size, shuffle=False)
+
+    # Evaluation of Jan's simple toeplitz example script
+    print("AUC scores computed using a single train_test_split with test_size = {}".format(test_size))
+
+    ### LDA
+    clf_lda = make_pipeline(LDA(),)
+    clf_lda.fit(X_train,y_train)
+
+    y_df = clf_lda.decision_function(X_test)
+    roc_auc_lda = roc_auc_score(y_test, y_df)
+    y_pred = clf_lda.predict(X_test)
+    bal_acc_auc_lda = balanced_accuracy_score(y_test, y_pred)
+
+    if only_auc:
+        print("AUC LDA: ", roc_auc_lda)
+    else:
+        print("LDA scores with channel prime data")
+        print("roc_auc: ",roc_auc_lda)
+        print("bal_acc_auc: ",bal_acc_auc_lda)
+
+    ### sLDA
+    clf_slda = make_pipeline(LDA(solver='lsqr', shrinkage='auto'),)
+    clf_slda.fit(X_train,y_train)
+
+    y_df = clf_slda.decision_function(X_test)
+    roc_auc_slda = roc_auc_score(y_test, y_df)
+    y_pred = clf_slda.predict(X_test)
+    bal_acc_auc_slda = balanced_accuracy_score(y_test, y_pred)
+
+    if only_auc:
+        print("AUC SLDA: ", roc_auc_slda)
+    else:     
+        print("\nsLDA scores with channel prime data")
+        print("roc_auc: ",roc_auc_slda)
+        print("bal_acc_auc: ",bal_acc_auc_slda)
+
+    ### BT-LDA
+    from toeplitzlda.classification import (
+        EpochsVectorizer,
+        ShrinkageLinearDiscriminantAnalysis,
+        ToeplitzLDA,
+    )
+
+    nch = (trials[0][0]).info["nchan"]
+    clf_btlda = make_pipeline(
+        ToeplitzLDA(n_channels=nch),
+    )
+    clf_btlda.fit(X_train,y_train)
+
+    y_df = clf_btlda.decision_function(X_test)
+    roc_auc_btlda = roc_auc_score(y_test, y_df)
+    y_pred = clf_btlda.predict(X_test)
+    bal_acc_auc_btlda = balanced_accuracy_score(y_test, y_pred)
+
+    if only_auc:
+        print("AUC BT-LDA: ", roc_auc_btlda)
+    else:    
+        print("\nBT-LDA scores with channel prime data")
+        print("roc_auc: ",roc_auc_btlda)
+        print("bal_acc_auc: ",bal_acc_auc_btlda)
+
+    # Eavluation of BCI course assignment 07, exercise 3
+    if plot_roc_curves:
+        fig, axes = plt.subplots(1, 3, figsize=(18, 6))  # 1 row, 3 columns
+
+        ### LDA
+        lda = LDA()
+        ldaclf = lda.fit(X_train, y_train)
+        fpr_lda, tpr_lda, thresholds_lda = metrics.roc_curve(y_test,ldaclf.decision_function(X_test)) # Compute signed distance of stimulus to decision boundary
+
+        metrics.RocCurveDisplay(fpr=fpr_lda, tpr=tpr_lda).plot(ax=axes[0])
+        axes[0].plot([0, 1],[0,1], '--') 
+        axes[0].legend(['ROC (area = %0.5f)' % metrics.auc(fpr_lda, tpr_lda), 'area = 0.5'], loc="lower right")
+        axes[0].set_title("AUC-ROC of LDA - using trials [{}:{}] - test_size = {}".format(start,stop,test_size))
+
+        ### Shrinkage LDA
+        slda = LDA(solver='lsqr',shrinkage = 'auto')
+        slda.fit(X_train, y_train)
+
+        fpr_slda, tpr_slda, thresholds_slda = metrics.roc_curve(y_test,slda.decision_function(X_test)) # Compute signed distance of stimulus to decision boundary
+        metrics.RocCurveDisplay(fpr=fpr_slda, tpr=tpr_slda).plot(ax=axes[1])
+        axes[1].plot([0, 1],[0,1], '--')
+        axes[1].legend(['ROC (area = %0.5f)' % metrics.auc(fpr_slda, tpr_slda), 'area = 0.5'], loc="lower right")
+        axes[1].set_title("AUC-ROC of sLDA - using trials [{}:{}] - test_size = {}".format(start,stop,test_size))
+
+        ### BT-LDA
+        nch = (trials[0][0]).info["nchan"]
+        # nch = 31 (for online sessions) and 61 for offline sessions
+        btlda = ToeplitzLDA(n_channels= nch) 
+        btlda.fit(X_train, y_train) 
+
+        fpr_btlda, tpr_btlda, thresholds_btlda = metrics.roc_curve(y_test,btlda.decision_function(X_test)) # Compute signed distance of stimulus to decision boundary
+        metrics.RocCurveDisplay(fpr=fpr_btlda, tpr=tpr_btlda).plot(ax=axes[2])
+        axes[2].plot([0, 1],[0,1], '--')
+        axes[2].legend(['ROC (area = %0.5f)' % metrics.auc(fpr_btlda, tpr_btlda), 'area = 0.5'], loc="lower right")
+        axes[2].set_title("AUC-ROC of BT-LDA - using trials [{}:{}] - test_size = {}".format(start,stop,test_size))
+        
+        plt.show()
+
+
+
+# Cross-validation instead of train_test_split
+# LDA vs SLDA vs BT-LDA on calibration data
+def compute_auc_with_cv(trials, start=0, stop=12, ival_bounds = np.array([0.1, 0.2, 0.3, 0.4, 0.5]), cv_folds = 4, show_mean = True, show_folds=False):
+    """
+    Computes and prints the average AUC score of LDA, SLDA and BTLDA on calibration data using cross-validation.
+
+    Calibration data is determined by a selection of trials [start - stop], e.g. [0-12]
+
+    Parameters:
+    - trials (list): list of all trials
+    - start (int): first trial to take as calibration data
+    - stop (int): last trial to take as calibration data. if None, then stop is the final trial of trials 
+    - ival_bounds (numpy array): time interval boundaries to average the raw calibration trials over
+    - cv_folds (int): number of folds 
+    - show_mean (boolean): if True (default), show mean AUC scores per classifier. if False, do not show this.
+    - show_folds (boolean): if True, show AUC scores per fold. if False (default), do not show this. 
+    """
+    
+    ### Feature extraction ------------------------------------------------------------
+
+    if stop is None:
+        stop = len(trials)
+        
+    raw_calibration_trials = trials[start:stop]
+    clf_ival_boundaries = ival_bounds
+
+    calibration_trials = [[get_jumping_means(iteration, clf_ival_boundaries) for iteration in trial] for trial in raw_calibration_trials]
+    calibration_trials_reshaped = [
+        [epochs.transpose(0, 2, 1) for epochs in trial] # make channel prime
+        for trial in calibration_trials
+    ]
+
+    # Reshaping to feed into classifier
+    calibration_stimuli = list()
+
+    # flatten into input for classifier
+    for trial in calibration_trials_reshaped:
+        for iteration in trial:
+            for epoch in iteration:
+                # flatten each epoch from 2D (n_time_ivals, n_channels) to 1D (n_time_ivals * n_channels) 
+                calibration_stimuli.append(epoch.reshape(-1)) 
+
+    calibration_stimuli = np.array(calibration_stimuli) # shape (samples,features),  e.g. (1038, 124)
+
+    calibration_labels = list()
+    # obtain flattened labels 
+    for trial in raw_calibration_trials:
+        for iteration in trial:
+            for event in iteration.events[:,2]:
+                calibration_labels.append(1 if event > 107 else 0)
+    
+    calibration_labels = np.array(calibration_labels) # contains only 0's and 1's
+    #print(calibration_labels.shape) # shape (samples,) e.g. (1038,)
+    X = calibration_stimuli
+    y = calibration_labels
+
+    ### LDA
+    clf_lda = make_pipeline(LDA(),)
+    auc_lda = cross_val_score(clf_lda, X, y, cv=cv_folds, scoring = 'roc_auc')
+    if show_folds:
+        print("AUC score of LDA, all {} folds: {}".format(cv_folds, auc_lda))
+    if show_mean:    
+        print("Mean AUC score of LDA: \t\t", auc_lda.mean())
+
+    ### SLDA
+    clf_slda = make_pipeline(LDA(solver='lsqr', shrinkage='auto'),)
+    auc_slda = cross_val_score(clf_slda, X, y, cv=cv_folds, scoring = 'roc_auc')
+    if show_folds:
+        print("AUC score of sLDA, all {} folds: {}".format(cv_folds, auc_slda))
+    if show_mean:     
+        print("Mean AUC score of sLDA: \t", auc_slda.mean())
+
+    ### BT-LDA
+    #nch = 31
+    nch = (trials[0][0]).info["nchan"]
+    clf_btlda = make_pipeline(
+        ToeplitzLDA(n_channels=nch),
+    )
+    auc_btlda = cross_val_score(clf_btlda, X, y, cv=cv_folds, scoring = 'roc_auc')
+    if show_folds:
+        print("AUC score of BT-LDA, all {} folds: {}".format(cv_folds, auc_btlda))
+    if show_mean:    
+        print("Mean AUC score of BT-LDA: \t", auc_btlda.mean())
+
