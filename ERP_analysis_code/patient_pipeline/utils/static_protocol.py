71a4415b288fefb11a36446338ef303c831fa243
Soz <soz.raheem@ru.nl>
2025-06-11 10:30:30 +0200

    Add code for Static Fixed BT-LDA (incl protocol for changing conditions), add results

---
+++ ERP_analysis_code/patient_pipeline/utils/static_protocol.py
@@ -0,0 +1,101 @@
+# Database
+static_protocol = {
+    # id : {
+    #       patient_nr,
+    #       last_session,
+    #       selection_for_calibration (e.g. "6D_long_350")
+    #       }
+
+    1: {
+        "patient_nr": 1, 
+        "last_session": 18,
+        "selection": "6D_long_350",
+        "changing_condition": False
+    },
+    2: {
+        "patient_nr": 2, 
+        "last_session": 14,
+        "selection": "6D_long_350",
+        "changing_condition": False
+    },
+    3: {
+        "patient_nr": 3, 
+        "last_session": 17,
+        "selection": "6D_short_250",
+        "changing_condition": True,
+
+        "changing_starter_sessions":{
+            3:"6D_short_250",
+            11: "HP_short_250"
+        }
+    },
+    4: {
+        "patient_nr": 4, 
+        "last_session": 20,
+        "selection": "6D_long_350",
+        "changing_condition": True,
+
+        "changing_starter_sessions":{
+            3:"6D_long_350",
+            12: "HP_long_350"
+        }
+    },
+    5: {
+        "patient_nr": 5, 
+        "last_session": 14,
+        "selection": "6D_short_250",
+        "changing_condition": True,
+        
+        "changing_starter_sessions":{
+            3:"6D_short_250",
+            8: "HP_long_350",
+            11: "HP_short_250"
+        }
+    },
+    6: {
+        "patient_nr": 6, 
+        "last_session": 16,
+        "selection": "6D_short_250",
+        "changing_condition": True,
+
+
+        "changing_starter_sessions":{
+            3:"6D_short_250",
+            9: "HP_short_250"
+        }
+    },
+    7: {
+        "patient_nr": 7, 
+        "last_session": 28,
+        "selection": "6D_long_350",
+        "changing_condition": False
+    },
+    8: {
+        "patient_nr": 8, 
+        "last_session": 19,
+        "selection": "6D_short_250",
+        "changing_condition": True,
+        
+        "changing_starter_sessions":{
+            4:"6D_short_250",
+            11: "HP_short_250"
+        }
+    },
+    9: {
+        "patient_nr": 9, 
+        "last_session": 18,
+        "selection": "6D_long_350",
+        "changing_condition": True,
+        
+        "changing_starter_sessions":{
+            3:"6D_long_350",
+            11: "HP_long_350",
+        }
+    },
+    10: {
+        "patient_nr": 10, 
+        "last_session": 16,
+        "selection": "6D_long_350",
+        "changing_condition": False
+    }
+}
